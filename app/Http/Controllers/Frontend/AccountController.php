67b066169765d74f875ae6fd7e4e07da3acaccdb
riddhi <riddhi@matrixbricks.com>
2025-03-25 12:20:31 +0530

    DomPDF and compsoer erros solve

--- app/Http/Controllers/Frontend/AccountController.php
+++ app/Http/Controllers/Frontend/AccountController.php
@@ -1,6 +1,7 @@
 <?php
 
-    namespace App\Http\Controllers\frontend;
+    namespace App\Http\Controllers\Frontend;
+
     
     use App\Http\Controllers\Controller;
     
--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\frontend;
+namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 
@@ -96,6 +96,7 @@ class HomeController extends Controller
     public function contact(Request $request)
     {
         return view('frontend.contact');
+
     }
 
     // === For Contact Us form submission
