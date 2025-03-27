8c4b31683bbbd7e39ea4b75f097541ee7fe4edc9
riddhi <riddhi@matrixbricks.com>
2025-03-27 11:26:46 +0530

    backend header changes

--- resources/views/components/backend/header.blade.php
+++ resources/views/components/backend/header.blade.php
@@ -45,10 +45,10 @@
               </li>
            
               <li class="profile-nav onhover-dropdown"> 
-                <div class="media profile-media"><img class="b-r-10" src="{{ asset('admin/assets/images/dashboard/profile_1.png') }}" alt="">
+                <div class="media profile-media"><img class="b-r-10" src="{{ asset('admin/assets/images/user/user.png') }}" alt="">
                   <div class="media-body d-xxl-block d-none box-col-none">
-                    <div class="d-flex align-items-center gap-2"> <span>Alex Mora </span><i class="middle fa fa-angle-down"> </i></div>
-                    <p class="mb-0 font-roboto">Admin</p>
+                    <div class="d-flex align-items-center gap-2"> <span>{{ Auth::user()->name }} </span><i class="middle fa fa-angle-down"> </i></div>
+                    <!-- <p class="mb-0 font-roboto">{{ Auth::user()->name }}</p> -->
                   </div>
                 </div>
                 <ul class="profile-dropdown onhover-show-div">
